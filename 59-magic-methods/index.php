99dbc2204175f4984d4a216a41b1031279b87bbc
Mehdi <Mehdi@DESKTOP-PKEHR98>
2024-11-27 10:46:35 +0330

    Add Magic Methods

---
+++ 59-magic-methods/index.php
@@ -0,0 +1,50 @@
+<?php
+
+class Page
+{
+    public array $attributes;
+
+    public function __construct()
+    {
+        $this->attributes = [
+            'id' => 7,
+            'title' => 'About us page'
+        ];
+    }
+
+    public function __get($key)
+    {
+//        var_dump($key);
+//        return 'title1';
+        if (isset($this->attributes[$key]))
+            return $this->attributes[$key];
+        return null;
+    }
+
+    public function __set($key,$value)
+    {
+//        var_dump($key);
+//        echo '<br>';
+//        var_dump($value);
+        $this->attributes[$key]=$value;
+    }
+
+    public function __unset($key)
+    {
+        var_dump("unset has been called for $key");
+//        unset($this->attributes[$key]);
+    }
+}
+
+$aboutUs = new Page();
+var_dump($aboutUs->title);
+var_dump($aboutUs->id);
+
+
+$aboutUs->content='This is contnet of about us page. you can know about us in this page';
+
+var_dump(isset($aboutUs->content));
+var_dump(isset($aboutUs->title));
+//var_dump($aboutUs->content);
+
+unset($aboutUs->content);
